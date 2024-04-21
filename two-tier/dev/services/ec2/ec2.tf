8a209b3d5b86c6b7b61f9039230fbb60a7d5d478
damianwojciechowski4 <110458027+damianwojciechowski4@users.noreply.github.com>
2024-04-21 20:18:39 +0200

    ec2 - subnet outputs

--- two-tier/dev/services/ec2/ec2.tf
+++ two-tier/dev/services/ec2/ec2.tf
@@ -2,13 +2,36 @@ data "aws_vpc" "dev" {
     id = var.vpc_id
 }
 
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
 
-data "aws_subnet_ids" "example" {
-    vpc_id = var.vpc_id
+  tags = {
+    Name = "Public subnet*"
+  }
+}
+
+data "aws_subnet" "public" {
+  for_each = toset(data.aws_subnets.public.ids)
+  id       = each.value
 }
 
 
-data "aws_subnet" "example" {
-    for_each = data.aws_subnet_ids.example.ids
-    id = each.value
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+
+  tags = {
+    Name = "Private subnet*"
+  }
 }
+
+data "aws_subnet" "private" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
--- two-tier/dev/services/ec2/outputs.tf
+++ two-tier/dev/services/ec2/outputs.tf
@@ -1,3 +1,8 @@
-output "subnet_cidr_blocks" {
-    value = [for s in data.aws_subnet.example : s.cidr_block]
+output "subnet_cidr_blocks_public" {
+    value = [for subnet in data.aws_subnet.public : subnet.cidr_block]
+}
+
+
+output "subnet_cidr_blocks_private" {
+    value = [for subnet in data.aws_subnet.private : subnet.cidr_block]
 }
