5f203fd485c7c1a763171cc9c7a60ca3090a078f
damianwojciechowski4 <110458027+damianwojciechowski4@users.noreply.github.com>
2024-04-21 19:54:09 +0200

    update infra

--- two-tier/dev/services/vpc/variables.tf
+++ two-tier/dev/services/vpc/variables.tf
@@ -1,11 +1,32 @@
-variable "backend_bucket" {
-    type = string
-    description = "S3 bucket for terraform state"
+variable "public_subnet_cidrs"{
+    description = "Public Subnet CIDR"
+    type = list(string)
+    default = ["10.100.0.0/24","10.100.1.0/24"]
 }
 
-variable "backend_dynamodb_table" {
-    type = string
-    description = "DynamoDB table for terraform state locking"
+variable "private_subnet_cidrs"{
+    description = "Private Subnet CIDR"
+    type = list(string)
+    default = ["10.100.100.0/24","10.100.101.0/24"]
 }
 
 
+
+variable "public_subnet_cidr_map" {
+  type    = map(string)
+  default = { "eu-central-1a" = "10.100.100.0/24", "eu-central-1b" = "10.100.1.0/24" }
+}
+
+variable "private_subnet_cidr_map" {
+  type    = map(string)
+  default = { "eu-central-1a" = "10.100.0.0/24", "eu-central-1b" = "10.100.101.0/24" }
+}
+
+
+
+
+variable "azs"{
+    type = list(string)
+    description = "Availability zones of eu-central"
+    default = ["eu-central-1a" , "eu-central-1b"]
+}
